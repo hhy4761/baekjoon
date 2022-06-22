22f0e0c25cf94941b689f33205f3ead09dfdfe53
hoyeon <ghdus1372@gmail.com>
2022-06-22 15:19:27 +0900

    [Gold IV] Title: 알파벳, Time: 7912 ms, Memory: 118048 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -0,0 +1,48 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
+
+def bfs(graph):
+    queue = set()
+    queue.add((0,0,graph[0][0]))
+    max_val = 0
+
+    while queue:
+        x,y,route = queue.pop()
+        max_val = max(max_val,len(route))   
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx >= R or nx < 0 or ny >= C or ny < 0:
+                continue
+            if graph[nx][ny] not in route:
+                queue.add((nx,ny,route+graph[nx][ny]))
+    print(max_val)
+
+def dfs(graph):
+    stack = deque()
+    stack.append((0,0,graph[0][0]))
+    max_val = 0
+
+    while stack:
+        x,y,route = stack.pop()
+        alphabet = set(route) 
+        max_val = max(max_val,len(alphabet))   
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx >= R or nx < 0 or ny >= C or ny < 0:
+                continue
+            if graph[nx][ny] not in alphabet:
+                stack.append((nx,ny,route+graph[nx][ny]))
+    print(max_val)
+
+
+R,C = map(int,input().split())
+graph = [[0]*C for i in range(R)]
+for i in range(R):
+    graph[i] = list(input().rstrip())
+
+bfs(graph)
