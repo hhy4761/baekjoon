1a098499f5c57df32cc814c417eb5c88b13258b1
hoyeon <ghdus1372@gmail.com>
2022-08-02 23:32:28 +0900

    [Silver IV] Title: 카드, Time: 124 ms, Memory: 3568 KB -BaekjoonHub

---
+++ 백준/Silver/11652. 카드/카드.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int n;
+vector<long long> v;
+
+int main(){
+    cin >> n;
+    while(n--){
+        long long num;
+        cin >> num;
+        v.push_back(num);
+    }
+    sort(v.begin(),v.end());
+    int left,right;
+    left = 0; right=0;
+    int max_cnt = 0;
+    long long max_val = INT64_MAX;
+    while(right < v.size()){
+        int cnt = 0;
+        while(right < v.size() and v[left]==v[right]){
+            right++;
+            cnt++;
+        }
+        if(max_cnt < cnt) {
+            max_cnt = cnt;
+            max_val = v[left];
+        }
+        left = right;
+    }
+    cout << max_val;
+}
