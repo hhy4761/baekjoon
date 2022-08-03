0497701147227cfb4389f1e5904c356cd655823f
hoyeon <ghdus1372@gmail.com>
2022-08-03 16:53:34 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int n,m;
+int ans[10];
+int nums[10];
+void dfs(int cur){
+    if(cur==m){
+        for(int i=0;i<m;i++)
+            cout << ans[i] << ' ';
+        cout << "\n";
+        return;
+    }
+    for(int i=0; i<n;i++){
+        if(cur > 0 and ans[cur-1] > nums[i]) continue;
+        ans[cur] = nums[i];
+        dfs(cur+1);
+    }
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> m;
+    for(int i=0; i<n;i++){
+        int num;
+        cin >> num;
+        nums[i] = num;
+    }
+    sort(nums,nums+n);
+    dfs(0);
+}
