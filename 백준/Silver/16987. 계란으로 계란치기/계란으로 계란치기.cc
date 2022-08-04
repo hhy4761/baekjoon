032e0f902030aae4176213fbbf13b5a9261f3b3a
hoyeon <ghdus1372@gmail.com>
2022-08-04 15:46:03 +0900

    [Silver I] Title: 계란으로 계란치기, Time: 44 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/16987. 계란으로 계란치기/계란으로 계란치기.cc
+++ 백준/Silver/16987. 계란으로 계란치기/계란으로 계란치기.cc
@@ -1,61 +1,42 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 #include <tuple>
 using namespace std;
 int n;
-bool broken[8];
-vector<pair<int,int>> v;
-int ans=0;
-void dfs(int cur, int cnt){
+int s[8],w[8];
+int ans = 0;
+int cnt = 0;
+
+void solve(int cur){
     if(cur == n){
-        ans = max(ans, cnt);
+        ans = max(ans,cnt);
         return;
     }
-    int cur_d,cur_w;
-    tie(cur_d,cur_w) = v[cur];
-    if(broken[cur]) dfs(cur+1,cnt);
-    else{
-        bool inloop = false;
-        for(int j=0; j<n; j++){
-            if(cur==j or broken[j]) continue;
-            inloop = true;
-            int next_d,next_w;
-            tie(next_d,next_w) = v[j];
-            v[cur] = {cur_d-next_w,cur_w};
-            v[j] = {next_d-cur_w,next_w};
-            if(v[cur].first <= 0){
-                broken[cur] = 1;
-                cnt++;
-            } 
-            if(v[j].first <= 0){
-                broken[j] = 1;
-                cnt++;
-            } 
-            dfs(cur+1, cnt);
-            v[cur] = {v[cur].first+next_w,cur_w};
-            v[j] = {v[j].first+cur_w,next_w};
-            if(broken[cur]){
-                broken[cur] = 0;
-                cnt--;
-            } 
-            if(broken[j]){
-                broken[j] = 0;
-                cnt--;
-            } 
-        }
-        if(!inloop) dfs(cur+1,cnt);
+    if(s[cur] <= 0 or cnt == n-1){
+        solve(cur+1);
+        return;
+    }
+    for(int i=0; i<n; i++){
+        if(s[i]<=0 or cur==i) continue;
+        s[cur] -= w[i];
+        s[i] -=w[cur];
+        if(s[cur] <= 0) cnt++;
+        if(s[i] <= 0) cnt++;
+        solve(cur+1);
+        if(s[cur] <=0) cnt--;
+        if(s[i] <=0) cnt--;
+        s[cur] += w[i];
+        s[i] += w[cur];
     }
 }
+
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     cin >> n;
     for(int i=0; i<n; i++){
-        int durability, weight;
-        cin >> durability >> weight;
-        v.push_back({durability,weight});
+        cin >> s[i] >> w[i];
     }
-    dfs(0,0);
+    solve(0);
     cout << ans;
 }
