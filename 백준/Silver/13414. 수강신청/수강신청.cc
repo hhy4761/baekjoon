9249245d7f34f16426f33b2cc0ecce414eaf99aa
hoyeon <ghdus1372@gmail.com>
2022-08-06 18:31:46 +0900

    [Silver III] Title: 수강신청, Time: 324 ms, Memory: 25364 KB -BaekjoonHub

--- 백준/Silver/13414. 수강신청/수강신청.cc
+++ 백준/Silver/13414. 수강신청/수강신청.cc
@@ -6,11 +6,6 @@ using namespace std;
 
 int k,l;
 unordered_map<string,int> m;
-
-bool cmp(pair<string,int> &a, pair<string,int> &b){
-    return a.second < b.second;
-}
-
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
@@ -25,7 +20,7 @@ int main(){
         else m.insert({num,i});
     }
     vector<pair<string,int>> v(m.begin(),m.end());
-    sort(v.begin(),v.end(),cmp);
+    sort(v.begin(),v.end(),[](auto &a,auto&b){return a.second < b.second;});
     for(int i=0; i<v.size(); i++){
         if(i==k) break;
         cout << v[i].first << "\n";
