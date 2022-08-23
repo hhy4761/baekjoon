8e35b36f3f9928263a0c1442f6c4a788375d54c1
hoyeon <ghdus1372@gmail.com>
2022-08-23 16:34:26 +0900

    [Gold IV] Title: 휴게소 세우기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.cc
+++ 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.cc
@@ -1,50 +1,38 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
 using namespace std;
 
-int main() {
-    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    vector<int> v;
-    int n, m, l, num;
-    int start, end, mid, ans = 1000;
+int n,m,l;
+int place[52];
+int ans = 1000;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     cin >> n >> m >> l;
+    place[0] = 0; place[n+1] = l;
+    for(int i=1; i<=n; i++)
+        cin >> place[i];
+    sort(place,place+n+2);
+    int left = 1, right = l-1;
+    while(left <= right){
+        int rest = 0;
+        int mid = (left+right) / 2;
 
-    v.push_back(0);
-    v.push_back(l);
-  
-    for(int i = 0 ; i < n ; i++) {
-        cin >> num;
-        v.push_back(num);
-    }
+        for(int i=1; i<=n+1; i++){
+            int dist = place[i] - place[i-1];
+            int cnt = dist / mid;
 
-    sort(v.begin(), v.end());
-
-    start = 1;
-    end = l - 1;
-
-    while(end >= start) {
-        int rest = 0; // 휴게소 개수
-        mid = (start + end) / 2;
-
-        for(int i = 1 ; i < v.size() ; i++) {
-            int dist = v[i] - v[i - 1];
-            int cnt = dist / mid; // 두 휴게소 사이에 설치 가능한 휴게소 개수
-
-            if(cnt > 0) { // 휴게소 설치 가능
-                if(dist % mid == 0) rest += cnt - 1;
+            if(cnt > 0 ){
+                if(dist % mid == 0) rest += cnt-1; 
                 else rest += cnt;
             }
         }
-
-        if(rest > m) start = mid + 1;
-        else {
-            end = mid - 1;
-            ans = min(mid, ans);
+        if(rest > m) left = mid + 1;
+        else{
+            right = mid - 1;
+            ans = min(mid,ans);
         }
     }
-
-    cout << ans;
-
+    cout << ans <<"\n";
     return 0;
 }
