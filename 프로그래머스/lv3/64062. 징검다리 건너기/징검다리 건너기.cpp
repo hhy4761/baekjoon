8122235b1e38903456829d1030d8e9b156d95949
hoyeon <ghdus1372@gmail.com>
2022-09-12 20:37:21 +0900

    [level 3] Title: 징검다리 건너기, Time: 3.86 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/lv3/64062. 징검다리 건너기/징검다리 건너기.cpp
+++ 프로그래머스/lv3/64062. 징검다리 건너기/징검다리 건너기.cpp
@@ -4,38 +4,36 @@
 #include <vector>
 
 using namespace std;
-
+/* 
+    통과할 수 없으면 F 가능하면 T 
+    이분 탐색 시 T~~~F 범위를 가지게 됨
+*/
 bool check(vector<int> S, int numFriends, int k){
-    for(int i=0; i<S.size(); i++)
-        S[i] -= numFriends;
-    
     int emptyCount = 0;
-    for(int i=0; i<S.size(); i++){
-        if(emptyCount >= k) return false;
-        if(S[i]<=0) emptyCount++;
+    for(auto& s : S){
+        if(s-(numFriends) <= 0) emptyCount++;
         else emptyCount = 0;
+        
+        if(emptyCount >= k) return false;
     }
     return true;
 }
 int ans;
 int solution(vector<int> stones, int k) {
-    int start = 0, end = 200000000;
-    while(start <= end){
-        int mid = (start+end)/2;
-        int emptyCount = 0;
-        for(auto& s : stones){
-            if(emptyCount >= k) break;
-            if(s-mid <= 0) emptyCount++;
-            else emptyCount = 0;
-        }
+    
+    int start = 1;
+    int end = *max_element(stones.begin(),stones.end());
+    int mid;
+    
+    while(start+1 < end){
+        mid = (start+end)/2;
         
-        if(emptyCount >= k){ // 징검다리 건넌 친구들 수 감소시켜야함.
-            end = mid - 1;
-            ans = mid;
+        if(check(stones,mid,k)){ // 징검다리 건넌 친구들 수 감소시켜야함.
+            start = mid;
         }
         else{ // 징검다리 건넌 친구들 수 증가시켜야함.
-            start = mid + 1;
+            end = mid;
         }
     }
-    return ans;
+    return end;
 }
